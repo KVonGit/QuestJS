25f0b4c5df09fd0551d1a188cf8566deb6754904
K.V. <kvonwin@gmail.com>
2024-12-05 21:26:56 -0600

    Delete docs for gh-pages

--- docs/game/code.js
+++
@@ -1,6 +0,0 @@
-"use strict"
-
-  findCmd('MetaHelp').script = function() {
-    msg('<p><a href="https://github.com/ThePix/QuestJS/wiki/">QuestJS Wiki</a></p><p><a href="https://github.com/ThePix/QuestJS/discussions/">QuestJS Discussions</a></p>');
-    return world.SUCCESS_NO_TURNSCRIPTS;
-  }
